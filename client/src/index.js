3308ebbe5c68c17be41212436af15fbffdcf8bcb
Jlong89 <jiaolong89@gmail.com>
2017-10-25 15:56:47 -0700

    bring in redux and introduce state

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import rockAdventures from './state/reducers';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
---
+++ client/src/state/reducers.js
@@ -0,0 +1,9 @@
+const initialState = {
+    
+}
+
+function rockAdventures(state = initialState, action) {
+    return state;
+}
+
+export default rockAdventures;
